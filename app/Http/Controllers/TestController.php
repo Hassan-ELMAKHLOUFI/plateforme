78f69ac1aa8d7964df26f84688466b7022f9e1d7
v0idFuncti0n <lakchouchanas@gmail.com>
2020-03-07 21:16:41 +0100

    Fix Ajouter champs description

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -49,7 +49,7 @@ class TestController extends Controller
             'duree' => $request->duree,
             'salle' => $request->salle,
             'date' => $request->date,
-            'description' => $request->discription
+            'discription' => $request->discription
         );
 
 
@@ -145,7 +145,7 @@ class TestController extends Controller
             'duree' => $request->duree,
             'salle' => $request->salle,
             'date' => $request->date,
-            'description' => $request->discription
+            'discription' => $request->discription
         ]);
 
         Test::findOrFail($request->test_id)->update($test);
